1263c523bfcc4e3283de83dd682f965d09bbd128
unknown <shane.kirkbride@gmail.com>
2015-04-10 21:02:20 -0600

    this is the first commit

---
+++ app/src/main/java/com/example/shane/spacechallenge1/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.shane.spacechallenge1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //upload button
+        Button upload_btn = (Button) this.findViewById(R.id.upload);
+        upload_btn.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+            // To open up a gallery browser
+                Toast.makeText(MainActivity.this, "Processing...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"),1);
+            }});
+
+
+        return true;
+    }
+    Uri currImageURI;
+    // To handle when an image is selected from the browser
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1) {
+                // currImageURI is the global variable I’m using to hold the content:
+                currImageURI = data.getData();
+                System.out.println("Current image Path is ----->" +
+                        getRealPathFromURI(currImageURI));
+                TextView tv_path = (TextView) findViewById(R.id.path);
+                tv_path.setText(getRealPathFromURI(currImageURI));
+            }
+        }
+    }
+
+    public String getRealPathFromURI(Uri contentUri) {
+        String [] proj={MediaStore.Images.Media.DATA};
+        android.database.Cursor cursor = getContentResolver().query( contentUri,
+                proj,     // Which columns to return
+                null,     // WHERE clause; which rows to return (all rows)
+                null,     // WHERE clause selection arguments (none)
+                null);     // Order-by clause (ascending by name)
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
